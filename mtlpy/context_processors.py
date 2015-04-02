6190c01eee6bb581153a7282d5c8768ae24718dd
Mathieu Leduc-Hamel <mlhamel@ajah.ca>
2015-04-02 16:22:09 -0400

    [#24] Fixed year in footer

--- mtlpy/context_processors.py
+++ mtlpy/context_processors.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
 from django.conf import settings
+
 
 def extra_context(request):
     return {
         "DISQUS_SITENAME": getattr(settings, "DISQUS_SITENAME"),
         "GOOGLE_ANALYTICS": getattr(settings, "GOOGLE_ANALYTICS"),
         "SITENAME": getattr(settings, "SITENAME"),
+        "YEAR": datetime.now().year
     }
