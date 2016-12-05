fb1b3d7967eb6ca98078fffe4f48c7991c9f7807
Pior Bastida <pior@pbastida.net>
2016-12-04 22:18:31 -0500

    Recreate migrations for Django 1.7

--- mtlpy/pages/migrations/0001_initial.py
+++ mtlpy/pages/migrations/0001_initial.py
@@ -1,49 +1,25 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'I18NFlatPage'
-        db.create_table(u'pages_i18nflatpage', (
-            (u'flatpage_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['flatpages.FlatPage'], unique=True, primary_key=True)),
-            ('language', self.gf('django.db.models.fields.CharField')(default='en', max_length=2)),
-        ))
-        db.send_create_signal(u'pages', ['I18NFlatPage'])
+    dependencies = [
+        ('flatpages', '0001_initial'),
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'I18NFlatPage'
-        db.delete_table(u'pages_i18nflatpage')
-
-
-    models = {
-        u'flatpages.flatpage': {
-            'Meta': {'ordering': "(u'url',)", 'object_name': 'FlatPage', 'db_table': "u'django_flatpage'"},
-            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'enable_comments': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'registration_required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'sites': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['sites.Site']", 'symmetrical': 'False'}),
-            'template_name': ('django.db.models.fields.CharField', [], {'max_length': '70', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'url': ('django.db.models.fields.CharField', [], {'max_length': '100', 'db_index': 'True'})
-        },
-        u'pages.i18nflatpage': {
-            'Meta': {'ordering': "(u'url',)", 'object_name': 'I18NFlatPage', '_ormbases': [u'flatpages.FlatPage']},
-            u'flatpage_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['flatpages.FlatPage']", 'unique': 'True', 'primary_key': 'True'}),
-            'language': ('django.db.models.fields.CharField', [], {'default': "'en'", 'max_length': '2'})
-        },
-        u'sites.site': {
-            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
-            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        }
-    }
-
-    complete_apps = ['pages']
+    operations = [
+        migrations.CreateModel(
+            name='I18NFlatPage',
+            fields=[
+                ('flatpage_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='flatpages.FlatPage')),
+                ('language', models.CharField(default=b'en', max_length=2, choices=[(b'fr', b'Fran\xc3\xa7ais'), (b'en', b'English')])),
+                ('translation', models.OneToOneField(null=True, blank=True, to='pages.I18NFlatPage', help_text=b'')),
+            ],
+            options={
+            },
+            bases=('flatpages.flatpage',),
+        ),
+    ]
