b0a902a9033993bfda7d536154be301d3d2451c3
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2017-04-14 13:07:48 -0400

    Adds integration tests

---
+++ mtlpy/test_views.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+
+from mtlpy import views
+from mtlpy.models import Sponsor
+
+
+class IntegrationTestCase(TestCase):
+    def setUp(self):
+        self.sponsor = Sponsor.objects.create(name='test', slug='test',
+            url='http://testserver.com', logo=None)
+
+    def test_home_page(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 301)
+        self.assertEqual(resp.url, 'http://testserver/en/')
+
+    def test_home_page_redirected(self):
+        resp = self.client.get('/en/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_styleguide(self):
+        resp = self.client.get('/en/styleguide/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_videos(self):
+        views.get_all_videos = lambda x: []
+        resp = self.client.get('/en/videos/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_sponsor_details(self):
+        resp = self.client.get('/en/sponsor/invalid/')
+        self.assertEqual(resp.status_code, 404)
+
+        resp = self.client.get('/en/sponsor/test/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_sponsorship(self):
+        resp = self.client.get('/en/sponsorship/')
+        self.assertEqual(resp.status_code, 200)
