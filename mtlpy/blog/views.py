c5eb97de40b3a7430b6dd0d42c30ee91f6d8756d
Pior Bastida <pior@pbastida.net>
2017-04-14 13:58:05 -0400

    Remove unused imports

--- mtlpy/blog/views.py
+++ mtlpy/blog/views.py
@@ -1,4 +1,3 @@
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib.admin.views.decorators import staff_member_required
--- mtlpy/views.py
+++ mtlpy/views.py
@@ -9,7 +9,7 @@ import pytz
 from django import forms
 from django.utils.translation import gettext_lazy as _
 from django.shortcuts import (render, render_to_response,
-                              get_object_or_404, redirect)
+                              get_object_or_404)
 from django.template import RequestContext
 from django.conf import settings
 from django.core.mail import send_mail
