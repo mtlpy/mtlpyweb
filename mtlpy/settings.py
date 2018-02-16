c7f37e50008ccc989609561f34dbed31ac7d4d14
Pior Bastida <pior@pbastida.net>
2018-02-15 19:32:55 -0500

    Enable local-memory cache in development

--- mtlpy/settings.py
+++ mtlpy/settings.py
@@ -22,7 +22,7 @@ DEBUG = env('DEBUG', cast=bool, default=False)
 SQL_DEBUG = env('SQL_DEBUG', cast=bool, default=False)
 
 CACHES = {
-    'default': env.cache_url(default='dummycache://'),
+    'default': env.cache_url(default='locmemcache://default'),
 }
 
 CONTACT_EMAILS = ['mtlpyteam@googlegroups.com']
