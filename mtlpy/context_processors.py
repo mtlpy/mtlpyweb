e9567e42931c0df3a670ef54faaa36db16ca3da4
Pior Bastida <pior@pbastida.net>
2018-04-28 15:44:41 -0400

    Remove Disqus comments

--- mtlpy/context_processors.py
+++ mtlpy/context_processors.py
@@ -5,7 +5,6 @@ from django.conf import settings
 
 def extra_context(request):
     return {
-        "DISQUS_SITENAME": getattr(settings, "DISQUS_SITENAME"),
         "GOOGLE_ANALYTICS": getattr(settings, "GOOGLE_ANALYTICS"),
         "SITENAME": getattr(settings, "SITENAME"),
         "YEAR": datetime.now().year
